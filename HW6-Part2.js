3c0cfbd3a898a7cb86dad4d10ab6376f5ed36a8e
lilikoishell <lilikoishell@gmail.com>
2025-11-10 22:47:13 -0800

    part2

--- HW6-Part2.js
+++ HW6-Part2.js
@@ -15,10 +15,17 @@ function searchCharacter() {
         }
     }
     if (count > 0) {
-            result.innerHTML = "The character '" + searchChar + "' appears " + count + " times.";
+        result.innerHTML = "The character '" + searchChar + "' appears " + count + " times.";
         result.style.color = "red";
-     } else {
-            result.innerHTML = "";
-            const newWindow = window.open("", "No Occurrences Found", "width=400,height=200");
-        }
+    } else {
+        result.innerHTML = "";
+        const newWindow = window.open("", "NotFoundWindow", "width=300,height=100,left=1200,top=100");
+        newWindow.document.write(
+            "<!DOCTYPE html>" +
+            "<html><head><title> Search Results </title></head><body>" +
+            "<p> Search character '" + searchChar + "' was not found in the content you typed. </p>" +
+            "<button onclick='window.close() '> Close </button>" +
+            "</body></html>"
+        );
+    }
 }
