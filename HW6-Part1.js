3ef90609be5d5ece654c0d35f476f93afe7ba773
lilikoishell <lilikoishell@gmail.com>
2025-11-10 21:34:17 -0800

    function

---
+++ HW6-Part1.js
@@ -0,0 +1,34 @@
+function submitForm() {
+    const input = document.getElementById('number').value;
+    const result = document.getElementById('result');
+    result.value = '';
+
+    const num = parseFloat(input);
+
+    if (isNaN(num)) {
+        result.value = 'Please enter a valid number.';
+        return;
+    }
+    const decimalPart = input.split('.')[1];
+    if (!decimalPart || decimalPart.length < 4) {
+        result.value = "You need to type a number with at least 4 decimal places"
+        return
+    }
+
+    const roundedInt = Math.round(num);
+    const sqrtRounded = Math.round(Math.sqrt(num));
+    const tenth = num.toFixed(1);
+    const hundredth = num.toFixed(2);
+    const thousandth = num.toFixed(3);
+
+    result.value =
+        "You typed number " + num + "\n" +
+        "Rounded to nearest integer: " + roundedInt + "\n" +
+        "Square root of rounded #: " + sqrtRounded + "\n" +
+        "Rounded to the nearest tenth: " + tenth + "\n" +
+        "Rounded to the nearest hundredth: " + hundredth + "\n" +
+        "Rounded to the nearest thousandth: " + thousandth + "\n";
+}
+function clearForm() {
+    document.getElementById('number').value = ""
+}
