4e1c315dda43c5a4a26987d1d5fc975eb7ba7d7e
lilikoishell <lilikoishell@gmail.com>
2025-11-18 15:28:51 -0800

    fixed

--- HW7-Part2.js
+++ HW7-Part2.js
@@ -1,11 +1,11 @@
-function getCookie(name, value) {
+function setCookie(name, value) {
     document.cookie = name + "=" + value + "; path=/";
 }
 
 function getCookie(name) {
     const cookieArray = document.cookie.split(";");
     for (let cookie of cookieArray) {
-        const parts = cookie.split("=");
+        const parts = cookie.trim().split("=");
         if (parts[0] === name) {
             return parts[1];
         }
@@ -19,22 +19,28 @@ function loadPage() {
     const username = getCookie("username");
     const area = document.getElementById("contentArea");
 
-    if (realname !== null || username !== null) {
+    if (realname !== null && username !== null) {
         area.innerHTML =
             "<h2> Welcome Back, " + realname + "!</h2>" +
-            "<p>Your username:" + username + "</p>";
+            "<p>Your username: " + username + "</p>";
     }
     else {
         area.innerHTML =
-            "Name: <input type='text' id='realname'><br></br>" +
-            "Username: <input type='text' id='username'><br></br>" +
-            <button id='saveBtn'> Save </button>;
+            "<label>Name: </label> <input type='text' id='realname'><br><br>" +
+            "<label>Username: </label><input type='text' id='username'><br><br>" +
+            "<button id='saveBtn'> Save </button>"+
+            "<p id='errormessage' style='color:red;'></p>";
     }
 }
 function saveUser() {
     const name = document.getElementById("realname").value;
     const user = document.getElementById("username").value;
+    const errormessage = document.getElementById("errormessage");
 
+    if (name === "" || user === "") {
+        errormessage.value = "Please fill in both fields!"
+        return
+    }
     setCookie("realname", name);
     setCookie("username", user);
 
@@ -45,5 +51,4 @@ document.addEventListener("click", function (event) {
     if (event.target.id === "saveBtn") {
         saveUser();
     }
-}
-)
+});
